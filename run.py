c176366e6d08579dbb717c7e7e0bd5a54f435fc2
Evgeny Karpovsky <ekarpovsky@gmail.com>
2021-10-16 11:03:46 +0300

    Improve run

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 # Usage:
-#   python run.py -i <input file> -o <output file> [-t True]
+#   python run.py -i <input file> -o <output file> -m <worsheet full path> [-d <fsm def full path>] [-s 'no'] [-t 'no']
 # 
 
 import argparse
@@ -39,37 +39,54 @@ def readJson(ffn):
     data = json.load(f)
   return data
 
+
+def writeJson(ffn, data):
+  with open(ffn, 'w') as fp:
+    json.dump(data, fp, indent=2)
+  return
+
+
 # Read configuration files
 def readConfig():
   return readJson('./cfg/fsm-cfg.json')
 
+
 def readImage(ffn):
   image = cv2.imread(ffn)
   return image
+
 
 def storeImage(input_ffn, out_path, image, idx):
   ffn = input_ffn.replace('\\', '/')
   fn, ext = ffn.split('/')[-1].split('.')
   ffn = "{}/result-{}-{}.{}".format(out_path, fn, idx, ext)
   cv2.imwrite(ffn, image)
+  return
 
-def run_by_step(runner, flow_meta):
+
+def run_by_step(runner, flow_meta, events):
   idx = 0
   while(True):
-    print('q - quit, r - run all')
-    print('waits for an event(next, prev, current):')
+    print('q - quit')
+    prompt = 'waits for an event {}:'.format(events)
+    print(prompt)
+    # print('waits for an event(next, prev, current):')
     event = input()
     if event == 'q':
       return
-
+    if event not in events:
+      continue
     step_meta = ''
     if idx < len(flow_meta):
       step_meta = flow_meta[idx]
     idx, cv2image = runner.run_step(event, step_meta)
     storeImage(kwargs["input"], kwargs["output"], cv2image, idx)
+  return
+
 
 def run_all(runner, flow_meta):
   runner.run_all(flow_meta)
+  return
 
 
 # Main function - the runner's client
@@ -77,6 +94,7 @@ def main(**kwargs):
   fsm_conf = readConfig()
   image = readImage(kwargs["input"])
   flow_meta = readJson(kwargs['meta'])
+  flow_meta.insert(0, {"stm": "glbstm.begin"})
   flow_meta.append({"stm": "glbstm.end"})
   for i, meta in enumerate(flow_meta):
     meta['id'] = i
@@ -85,7 +103,8 @@ def main(**kwargs):
   else:
     fc = FlowConverter(flow_meta)
     fsm_def = fc.convert()
-
+    # if kwargs['trace'] == 'yes':
+    #   writeJson('../data/fsm-def/fsm-def-edge.json', fsm_def)
   # Create the runner
   rn = Runner()
   # Recreate engine when a flow meta changed
@@ -97,7 +116,9 @@ def main(**kwargs):
   if kwargs["step"] == "no":
     run_all(rn, flow_meta)
   else:
-    run_by_step(rn, flow_meta)   
+    run_by_step(rn, flow_meta, fsm_conf.get('events'))   
+  return
+
 
 # Entry point
 if __name__ == "__main__":
