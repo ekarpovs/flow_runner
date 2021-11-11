cb0d2deda2f52846677cfaec3d57d9a819b532a5
Evgeny Karpovsky <ekarpovsky@gmail.com>
2021-11-11 08:44:24 +0200

    Integration wiyh flow manager

--- flow_runner/runner.py
+++ flow_runner/runner.py
@@ -4,20 +4,14 @@ from gfsm.fsm import FSM
 from frfsm.frfsm import Frfsm
 from flow_model import FlowModel, FlowItemModel
 
-# from .storage import Storage
-
 class Runner():
   def __init__(self):
+    self._storage: FlowStorage = None
     self._fsm = FSM('cntx_test')
     self._frfsm: Frfsm = None
-    self._storage: FlowStorage = None
     return
 
 # Properties
-  @property
-  def frfrsm(self) -> Frfsm:
-    return self._frfsm
-
   @property
   def storage(self) -> FlowStorage:
     return self._storage
@@ -26,11 +20,6 @@ class Runner():
   def storage(self, storage: FlowStorage) -> None:
     self._storage = storage
 
-
-  @property
-  def number_of_states(self) -> int:
-    return self._frfsm.number_of_states
-
   @property
   def initialized(self) -> bool:
     return self._frfsm is not None
@@ -38,7 +27,7 @@ class Runner():
   # runtime
   #  
   @property
-  def state_id(self) -> int:
+  def state_idx(self) -> int:
      return self._fsm.current_state_id
 
 # Methods
@@ -48,26 +37,38 @@ class Runner():
     self._frfsm = Frfsm(fsm_conf, fsm_def)
     return
 
-
-  def start(self):
+  def start(self) -> None:
     self._fsm.start(self._frfsm.impl)   
     return
 
-  def map_event_name(self, event):
-    return event
-
-  def dispatch_event(self, event, flow_item:FlowItemModel):
-    if event == 'next':
-  	  self.dispatch_next(flow_item)
-    elif event == 'prev':
-  	  self.dispatch_prev(flow_item)
-    else:
-  	  self.dispatch_current(flow_item)
+  def run_all(self, model: FlowModel) -> None:
+    n = self._frfsm.number_of_states
+    idx = 0
+    while (idx <= n-1):
+      flow_item = model.get_item(idx)
+      idx = idx+1
+      self.run_step('next', flow_item)
     return
 
-  def dispatch_next(self, flow_item: FlowItemModel) -> None:
+  def run_step(self, event, flow_item: FlowItemModel):
+    self._dispatch_event(event, flow_item)
+    return
+
+  def _map_event_name(self, event: str) -> str:
+    return event
+
+  def _dispatch_event(self, event, flow_item:FlowItemModel):
+    if event == 'next':
+  	  self._dispatch_next(flow_item)
+    elif event == 'prev':
+  	  self._dispatch_prev(flow_item)
+    else:
+  	  self._dispatch_current(flow_item)
+    return
+
+  def _dispatch_next(self, flow_item: FlowItemModel) -> None:
     event = 'next'
-    event = self.map_event_name(event)
+    event = self._map_event_name(event)
     self._fsm.set_user_data("params", flow_item.params)
     state_id = self._fsm.current_state_name
     data = self.storage.get_state_input_data(state_id)
@@ -85,16 +86,14 @@ class Runner():
     self.storage.set_state_input_refs(state_id, out_refs)
     return
 
-
-  def dispatch_prev(self, flow_item: FlowItemModel):
+  def _dispatch_prev(self, flow_item: FlowItemModel):
     event = 'prev'
     state_id = self._fsm.current_state_name
     self.storage.clean_state_output_data(state_id)
     self._fsm.dispatch(event)
     return
 
-
-  def dispatch_current(self, flow_item: FlowItemModel):
+  def _dispatch_current(self, flow_item: FlowItemModel):
     event = 'current'
     self._fsm.set_user_data("params", flow_item.params)
     state_id = self._fsm.current_state_name
@@ -107,20 +106,4 @@ class Runner():
     # Strore output data of the state
     data = self._fsm.get_user_data("data")
     self.storage.set_state_output_data(state_id, data)
-    return
-
-
-
-  def run_step(self, event, flow_item: FlowItemModel):
-    self.dispatch_event(event, flow_item)
-    return
-
-
-  def run_all(self, model: FlowModel):
-    n = self.number_of_states
-    idx = 0
-    while (idx <= n-1):
-      flow_item = model.get_item(idx)
-      idx = idx+1
-      self.run_step('next', flow_item)
     return
